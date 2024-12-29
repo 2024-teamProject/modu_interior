32d14d8aba283443d87f722420ea27464681121e
PARKYUNSU <125172299+PARKYUNSU@users.noreply.github.com>
2024-12-29 20:43:55 +0900

    commit

--- Image_retrieval/SimSiam/gdownload.py
+++
@@ -1,7 +0,0 @@
-import gdown
-
-file_id = "1Y5cV5IkWvQeM-js7aQZZAcjGXzcdHm_1"
-url = f"https://drive.google.com/uc?id={file_id}"
-output = "pretrain_ckpt.pth.tar"
-
-gdown.download(url, output, quiet=False)
